be153211084f24fed11efcc4ae3c7c1154fb75ee
sergey <sergey.demesh98@gmail.com>
2024-09-08 01:40:19 +0300

    add button togle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import {useTg} from "./hooks/useTg";
 
 const tg = window.Telegram.WebApp
 
 const App = () => {
+
+  const {onToggleButton} = useTg()
 
   useEffect(() => {
     tg.ready();
@@ -15,6 +18,7 @@ const App = () => {
   return (
     <div className="app">
       <Header></Header>
+      <button onClick={onToggleButton}>Toggle Tg</button>
     </div>
   );
 }
