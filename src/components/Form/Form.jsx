2455e4f7bbfba58bcfe878e2a3bd135204e9147e
sergey <sergey.demesh98@gmail.com>
2024-09-28 22:36:46 +0300

    add form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Form.css'
+import {useTg} from "../../hooks/useTg";
 
 const Form = () => {
     const [country, setCountry] = React.useState('');
     const [city, setCity] = React.useState('');
     const [subject, setSubject] = React.useState('');
+    const {tg} = useTg()
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            'text': 'Отправить данные',
+        })
+    }, []);
+
+    useEffect(() => {
+        if (!country || !city) {
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+        }
+    }, [country, city]);
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value);
+    }
+
+    const onChangeCity = (e) => {
+        setCity(e.target.value);
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value);
+    }
 
     return (
         <div className={"form"}>
             <h3>Введите данные</h3>
-            <input className={'input'} type="text" placeholder={'Страна'}/>
-            <input className={'input'} type="text" placeholder={'Город'}/>
-            <select className={'select'}>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Город'}
+                value={city}
+                onChange={onChangeCity}
+            />
+            <select className={'select'} value={subject} onChange={onChangeSubject}>
                 <option value={'physical'}>Физ. лицо</option>
                 <option value={'legal'}>Юр. лицо</option>
             </select>
