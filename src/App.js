a2140de8e84d07b8aa0e3ec4bf7b6db38d7e82bd
sergey <sergey.demesh98@gmail.com>
2024-09-28 23:31:28 +0300

    add toml file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import './App.css';
-
 import Header from './components/Header/Header';
 import {Route, Routes} from "react-router-dom";
 import ProductList from "./components/ProductList/ProductList";
